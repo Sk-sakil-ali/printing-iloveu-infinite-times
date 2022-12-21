dc8b01b22882e781a6f6875b77b98a305ab186e6
Sk-sakil-ali <121190073+Sk-sakil-ali@users.noreply.github.com>
2022-12-21 23:30:50 +0530

    Create iloveu.cpp

---
+++ iloveu.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    //This is a simple program that will print I LOVE YOU infinite times; send this to your loved ones
+   
+while(1>0){
+    cout<<"I LOVE YOU"<<endl;
+}
+return 0;
+}
